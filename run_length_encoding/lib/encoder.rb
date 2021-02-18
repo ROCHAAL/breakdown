97fd78ab8762cea64bb1b5d15179175259262eec
patricia <prmonday@gmail.com>
2021-02-18 17:57:03 +0000

    get the length and the first letter method

--- run_length_encoding/lib/encoder.rb
+++ run_length_encoding/lib/encoder.rb
@@ -1,2 +1,13 @@
 class Encoder
+  def get_length(str)
+    str.length
+  end
+
+  def get_first_letter(str)
+    str[0]
+  end
 end
+
+
+
+#string input output the lenth and the duplicated letter in the string
